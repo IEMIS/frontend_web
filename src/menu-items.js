9dd7d3d8507dcbff95244631dc227e296396525b
abduljeleelng <abduljeleelng@gmail.com>
2021-02-16 22:11:56 +0100

    menu names

--- src/menu-items.js
+++ src/menu-items.js
@@ -10,20 +10,75 @@ export default {
                     id: 'dashboard',
                     title: 'Dashboard',
                     type: 'item',
-                    url: '/dashboard/default',
+                    url: '/admin',
                     icon: 'feather icon-home',
                 }
             ]
         },
         {
-            id: 'ui-element',
-            title: 'UI ELEMENT',
+            id: 'districts',
+            title: 'districts board',
+            type: 'group',
+            icon: 'icon-navigation',
+            children: [
+                {
+                    id: 'districts',
+                    title: 'Districts',
+                    type: 'item',
+                    url: '/admin/districts',
+                    icon: 'feather icon-home',
+                }
+            ]
+        },
+        {
+            id: 'district-list',
+            title: 'Districts',
             type: 'group',
             icon: 'icon-ui',
             children: [
                 {
                     id: 'basic',
-                    title: 'Component',
+                    title: 'District A',
+                    type: 'collapse',
+                    icon: 'feather icon-box',
+                    children: [
+                        {
+                            id: 'button',
+                            title: 'School A',
+                            type: 'item',
+                            url: '/admin/districta/schoola'
+                        },
+                        {
+                            id: 'badges',
+                            title: 'School B',
+                            type: 'item',
+                            url: '/admin/districta/schoolb'
+                        },
+                    ]
+                },
+                {
+                    id: 'basic',
+                    title: 'District B',
+                    type: 'collapse',
+                    icon: 'feather icon-box',
+                    children: [
+                        {
+                            id: 'button',
+                            title: 'School A',
+                            type: 'item',
+                            url: '/admin/districtb/schoola'
+                        },
+                        {
+                            id: 'badges',
+                            title: 'School B',
+                            type: 'item',
+                            url: '/admin/districtb/schoolb'
+                        },
+                    ]
+                },
+                {
+                    id: 'basic',
+                    title: 'District C',
                     type: 'collapse',
                     icon: 'feather icon-box',
                     children: [
@@ -31,183 +86,24 @@ export default {
                             id: 'button',
                             title: 'Button',
                             type: 'item',
-                            url: '/basic/button'
+                            url: '/admin/districtc/schoolb'
                         },
                         {
                             id: 'badges',
                             title: 'Badges',
                             type: 'item',
-                            url: '/basic/badges'
+                            url: '/admin/districtc/schoolb'
                         },
-                        {
-                            id: 'breadcrumb-pagination',
-                            title: 'Breadcrumb & Pagination',
-                            type: 'item',
-                            url: '/basic/breadcrumb-paging'
-                        },
-                        {
-                            id: 'collapse',
-                            title: 'Collapse',
-                            type: 'item',
-                            url: '/basic/collapse'
-                        },
-                        {
-                            id: 'tabs-pills',
-                            title: 'Tabs & Pills',
-                            type: 'item',
-                            url: '/basic/tabs-pills'
-                        },
-                        {
-                            id: 'typography',
-                            title: 'Typography',
-                            type: 'item',
-                            url: '/basic/typography'
-                        }
                     ]
-                }
-            ]
-        },
-        {
-            id: 'ui-forms',
-            title: 'Forms & Tables',
-            type: 'group',
-            icon: 'icon-group',
-            children: [
-                {
-                    id: 'form-basic',
-                    title: 'Form Elements',
-                    type: 'item',
-                    url: '/forms/form-basic',
-                    icon: 'feather icon-file-text'
-                },
-                {
-                    id: 'bootstrap',
-                    title: 'Table',
-                    type: 'item',
-                    icon: 'feather icon-server',
-                    url: '/tables/bootstrap'
-                }
-            ]
-        },
-        {
-            id: 'chart-maps',
-            title: 'Chart & Maps',
-            type: 'group',
-            icon: 'icon-charts',
-            children: [
-                {
-                    id: 'charts',
-                    title: 'Charts',
-                    type: 'item',
-                    icon: 'feather icon-pie-chart',
-                    url: '/charts/nvd3'
-                },
-                {
-                    id: 'maps',
-                    title: 'Map',
-                    type: 'item',
-                    icon: 'feather icon-map',
-                    url: '/maps/google-map'
                 }
             ]
         },
         {
             id: 'pages',
-            title: 'Pages',
+            title: 'Footer',
             type: 'group',
             icon: 'icon-pages',
             children: [
-                {
-                    id: 'auth',
-                    title: 'Authentication',
-                    type: 'collapse',
-                    icon: 'feather icon-lock',
-                    badge: {
-                        title: 'New',
-                        type: 'label-danger'
-                    },
-                    children: [
-                        {
-                            id: 'signup-1',
-                            title: 'Sign up',
-                            type: 'item',
-                            url: '/auth/signup-1',
-                            target: true,
-                            breadcrumbs: false
-                        },
-                        {
-                            id: 'signin-1',
-                            title: 'Sign in',
-                            type: 'item',
-                            url: '/auth/signin-1',
-                            target: true,
-                            breadcrumbs: false
-                        }
-                    ]
-                },
-
-                {
-                    id: 'sample-page',
-                    title: 'Sample Page',
-                    type: 'item',
-                    url: '/sample-page',
-                    classes: 'nav-item',
-                    icon: 'feather icon-sidebar'
-                },
-                {
-                    id: 'docs',
-                    title: 'Documentation',
-                    type: 'item',
-                    url: '/docs',
-                    classes: 'nav-item',
-                    icon: 'feather icon-help-circle'
-                },
-                {
-                    id: 'menu-level',
-                    title: 'Menu Levels',
-                    type: 'collapse',
-                    icon: 'feather icon-menu',
-                    children: [
-                        {
-                            id: 'menu-level-1.1',
-                            title: 'Menu Level 1.1',
-                            type: 'item',
-                            url: '#!',
-                        },
-                        {
-                            id: 'menu-level-1.2',
-                            title: 'Menu Level 2.2',
-                            type: 'collapse',
-                            children: [
-                                {
-                                    id: 'menu-level-2.1',
-                                    title: 'Menu Level 2.1',
-                                    type: 'item',
-                                    url: '#',
-                                },
-                                {
-                                    id: 'menu-level-2.2',
-                                    title: 'Menu Level 2.2',
-                                    type: 'collapse',
-                                    children: [
-                                        {
-                                            id: 'menu-level-3.1',
-                                            title: 'Menu Level 3.1',
-                                            type: 'item',
-                                            url: '#',
-                                        },
-                                        {
-                                            id: 'menu-level-3.2',
-                                            title: 'Menu Level 3.2',
-                                            type: 'item',
-                                            url: '#',
-                                        }
-                                    ]
-                                }
-                            ]
-                        }
-                    ]
-                },
                 {
                     id: 'disabled-menu',
                     title: 'Disabled Menu',
@@ -215,21 +111,7 @@ export default {
                     url: '#',
                     classes: 'nav-item disabled',
                     icon: 'feather icon-power'
-                },
-                /*{
-                    id: 'buy-now',
-                    title: 'Buy Now',
-                    type: 'item',
-                    icon: 'feather icon-user',
-                    classes: 'nav-item',
-                    url: 'https://codedthemes.com',
-                    target: true,
-                    external: true,
-                    badge: {
-                        title: 'v1.0',
-                        type: 'label-primary'
-                    }
-                }*/
+                }
             ]
         }
     ]
