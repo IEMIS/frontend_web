4c76307512b23bb9cc3d80f1d8f43273e0b9b0ef
abduljeleelng <abduljeleelng@gmail.com>
2021-03-01 15:50:43 +0100

    integrate chart graph by Ife

--- src/screen/admin/Dashboard.js
+++ src/screen/admin/Dashboard.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import {Row, Col, Card, Table, Tabs, Tab} from 'react-bootstrap';
+import {Row, Col, Card, Tabs, Tab} from 'react-bootstrap';
+import NVD3Chart from 'react-nvd3';
+
+
+
 
 import Aux from "../../hoc/_Aux";
 import DEMO from "../../store/constant";
@@ -8,8 +12,77 @@ import avatar1 from '../../assets/images/user/avatar-1.jpg';
 import avatar2 from '../../assets/images/user/avatar-2.jpg';
 import avatar3 from '../../assets/images/user/avatar-3.jpg';
 
+
+function generateNumber(min, max) {
+    return Math.floor(Math.random()*(max-min+1)+min)
+}
+
+function getDatum() {
+    let sin = [],
+        sin2 = [],
+        sin3 = [];
+
+    const len =  35 + (Math.random() * (70-35));
+    for (let i = 0; i < len; i++) {
+        sin.push({
+            'x': i,
+            'y': generateNumber(0, 60)
+        });
+        sin2.push({
+            'x': i,
+            'y': generateNumber(0, 100)
+        });
+        sin3.push({
+            'x': i,
+            'y': generateNumber(0, 30)
+        });
+    }
+    return [
+        {
+            values: sin,
+            key: 'Stream #0',
+            color: '#A389D4'
+        },
+        {
+            values: sin3,
+            key: 'Stream #1',
+            color: '#04a9f5'
+        },
+        {
+            values: sin2,
+            key: 'Stream #3',
+            color: '#1de9b6',
+            area: true
+        }
+    ];
+}
+
+const datum1 = [
+    {key: "One", y: 29, color: "#ff8a65"},
+    {key: "Two", y: 0, color: "#f4c22b"},
+    {key: "Three", y: 32, color: "#04a9f5"},
+    {key: "Four", y: 196, color: "#3ebfea"},
+    {key: "Five", y: 2, color: "#4F5467"},
+    {key: "Six", y: 98, color: "#1de9b6"},
+    {key: "Seven", y: 13, color: "#a389d4"},
+    {key: "Eight", y: 5, color: "#FE8A7D"}
+];
+
+const datum2 = [
+    {key: "One", y: 29, color: "#ff8a65"},
+    {key: "Two", y: 0, color: "#f4c22b"},
+    {key: "Three", y: 32, color: "#04a9f5"},
+    {key: "Four", y: 196, color: "#3ebfea"},
+    {key: "Five", y: 2, color: "#4F5467"},
+    {key: "Six", y: 98, color: "#1de9b6"},
+    {key: "Seven", y: 13, color: "#a389d4"},
+    {key: "Eight", y: 5, color: "#FE8A7D"}
+];
+
 class Dashboard extends React.Component {
     render() {
+
+        const data = getDatum();
         const tabContent = (
             <Aux>
                 <div className="media friendlist-box align-items-center justify-content-center m-b-20">
@@ -68,6 +141,7 @@ class Dashboard extends React.Component {
                 </div>
             </Aux>
         );
+
 
         return (
             <Aux>
@@ -132,68 +206,10 @@ class Dashboard extends React.Component {
                     <Col md={6} xl={8}>
                         <Card className='Recent-Users'>
                             <Card.Header>
-                                <Card.Title as='h5'>Recent Users</Card.Title>
+                                <Card.Title as='h5'>Students Summary</Card.Title>
                             </Card.Header>
                             <Card.Body className='px-0 py-2'>
-                                <Table responsive hover>
-                                    <tbody>
-                                    <tr className="unread">
-                                        <td><img className="rounded-circle" style={{width: '40px'}} src={avatar1} alt="activity-user"/></td>
-                                        <td>
-                                            <h6 className="mb-1">Isabella Christensen</h6>
-                                            <p className="m-0">Lorem Ipsum is simply dummy text of…</p>
-                                        </td>
-                                        <td>
-                                            <h6 className="text-muted"><i className="fa fa-circle text-c-green f-10 m-r-15"/>11 MAY 12:56</h6>
-                                        </td>
-                                        <td><a href={DEMO.BLANK_LINK} className="label theme-bg2 text-white f-12">Reject</a><a href={DEMO.BLANK_LINK} className="label theme-bg text-white f-12">Approve</a></td>
-                                    </tr>
-                                    <tr className="unread">
-                                        <td><img className="rounded-circle" style={{width: '40px'}} src={avatar2} alt="activity-user"/></td>
-                                        <td>
-                                            <h6 className="mb-1">Mathilde Andersen</h6>
-                                            <p className="m-0">Lorem Ipsum is simply dummy text of…</p>
-                                        </td>
-                                        <td>
-                                            <h6 className="text-muted"><i className="fa fa-circle text-c-red f-10 m-r-15"/>11 MAY 10:35</h6>
-                                        </td>
-                                        <td><a href={DEMO.BLANK_LINK} className="label theme-bg2 text-white f-12">Reject</a><a href={DEMO.BLANK_LINK} className="label theme-bg text-white f-12">Approve</a></td>
-                                    </tr>
-                                    <tr className="unread">
-                                        <td><img className="rounded-circle" style={{width: '40px'}} src={avatar3} alt="activity-user"/></td>
-                                        <td>
-                                            <h6 className="mb-1">Karla Sorensen</h6>
-                                            <p className="m-0">Lorem Ipsum is simply dummy text of…</p>
-                                        </td>
-                                        <td>
-                                            <h6 className="text-muted"><i className="fa fa-circle text-c-green f-10 m-r-15"/>9 MAY 17:38</h6>
-                                        </td>
-                                        <td><a href={DEMO.BLANK_LINK} className="label theme-bg2 text-white f-12">Reject</a><a href={DEMO.BLANK_LINK} className="label theme-bg text-white f-12">Approve</a></td>
-                                    </tr>
-                                    <tr className="unread">
-                                        <td><img className="rounded-circle" style={{width: '40px'}} src={avatar1} alt="activity-user"/></td>
-                                        <td>
-                                            <h6 className="mb-1">Ida Jorgensen</h6>
-                                            <p className="m-0">Lorem Ipsum is simply dummy text of…</p>
-                                        </td>
-                                        <td>
-                                            <h6 className="text-muted f-w-300"><i className="fa fa-circle text-c-red f-10 m-r-15"/>19 MAY 12:56</h6>
-                                        </td>
-                                        <td><a href={DEMO.BLANK_LINK} className="label theme-bg2 text-white f-12">Reject</a><a href={DEMO.BLANK_LINK} className="label theme-bg text-white f-12">Approve</a></td>
-                                    </tr>
-                                    <tr className="unread">
-                                        <td><img className="rounded-circle" style={{width: '40px'}} src={avatar2} alt="activity-user"/></td>
-                                        <td>
-                                            <h6 className="mb-1">Albert Andersen</h6>
-                                            <p className="m-0">Lorem Ipsum is simply dummy text of…</p>
-                                        </td>
-                                        <td>
-                                            <h6 className="text-muted"><i className="fa fa-circle text-c-green f-10 m-r-15"/>21 July 12:56</h6>
-                                        </td>
-                                        <td><a href={DEMO.BLANK_LINK} className="label theme-bg2 text-white f-12">Reject</a><a href={DEMO.BLANK_LINK} className="label theme-bg text-white f-12">Approve</a></td>
-                                    </tr>
-                                    </tbody>
-                                </Table>
+                                <NVD3Chart type="multiBarChart" datum={data} x="x" y="y" height={380} showValues groupSpacing={0.5} />
                             </Card.Body>
                         </Card>
                     </Col>
@@ -243,27 +259,12 @@ class Dashboard extends React.Component {
                             <Card.Body className='border-bottom'>
                                 <div className="row align-items-center justify-content-center">
                                     <div className="col-auto">
-                                        <i className="fa fa-facebook text-primary f-36"/>
+                                        {/*<i className="fa fa-facebook text-primary f-36"/>*/}
+                                        <h5>Teaching staff</h5>
                                     </div>
                                     <div className="col text-right">
                                         <h3>12,281</h3>
-                                        <h5 className="text-c-green mb-0">+7.2% <span className="text-muted">Total Likes</span></h5>
-                                    </div>
-                                </div>
-                            </Card.Body>
-                            <Card.Body>
-                                <div className="row align-items-center justify-content-center card-active">
-                                    <div className="col-6">
-                                        <h6 className="text-center m-b-10"><span className="text-muted m-r-5">Target:</span>35,098</h6>
-                                        <div className="progress">
-                                            <div className="progress-bar progress-c-theme" role="progressbar" style={{width: '60%', height: '6px'}} aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"/>
-                                        </div>
-                                    </div>
-                                    <div className="col-6">
-                                        <h6 className="text-center  m-b-10"><span className="text-muted m-r-5">Duration:</span>350</h6>
-                                        <div className="progress">
-                                            <div className="progress-bar progress-c-theme2" role="progressbar" style={{width: '45%', height: '6px'}} aria-valuenow="45" aria-valuemin="0" aria-valuemax="100"/>
-                                        </div>
+                                        {/*<h5 className="text-c-green mb-0">+7.2% <span className="text-muted">Total Likes</span></h5>*/}
                                     </div>
                                 </div>
                             </Card.Body>
@@ -274,27 +275,11 @@ class Dashboard extends React.Component {
                             <Card.Body className='border-bottom'>
                                 <div className="row align-items-center justify-content-center">
                                     <div className="col-auto">
-                                        <i className="fa fa-twitter text-c-blue f-36"/>
+                                        <h5>Non-Teaching Staff</h5>
                                     </div>
                                     <div className="col text-right">
                                         <h3>11,200</h3>
-                                        <h5 className="text-c-purple mb-0">+6.2% <span className="text-muted">Total Likes</span></h5>
-                                    </div>
-                                </div>
-                            </Card.Body>
-                            <Card.Body>
-                                <div className="row align-items-center justify-content-center card-active">
-                                    <div className="col-6">
-                                        <h6 className="text-center m-b-10"><span className="text-muted m-r-5">Target:</span>34,185</h6>
-                                        <div className="progress">
-                                            <div className="progress-bar progress-c-green" role="progressbar" style={{width: '40%', height: '6px'}} aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"/>
-                                        </div>
-                                    </div>
-                                    <div className="col-6">
-                                        <h6 className="text-center  m-b-10"><span className="text-muted m-r-5">Duration:</span>800</h6>
-                                        <div className="progress">
-                                            <div className="progress-bar progress-c-blue" role="progressbar" style={{width: '70%', height: '6px'}} aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"/>
-                                        </div>
+                                        
                                     </div>
                                 </div>
                             </Card.Body>
@@ -305,29 +290,32 @@ class Dashboard extends React.Component {
                             <Card.Body className='border-bottom'>
                                 <div className="row align-items-center justify-content-center">
                                     <div className="col-auto">
-                                        <i className="fa fa-google-plus text-c-red f-36"/>
+                                        <h5>Total Staff</h5>
                                     </div>
                                     <div className="col text-right">
                                         <h3>10,500</h3>
-                                        <h5 className="text-c-blue mb-0">+5.9% <span className="text-muted">Total Likes</span></h5>
                                     </div>
                                 </div>
                             </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col md={6} xl={6}>
+                        <Card>
+                            <Card.Header>
+                                <Card.Title as='h5'>Students Attendance Weekly Report</Card.Title>
+                            </Card.Header>
                             <Card.Body>
-                                <div className="row align-items-center justify-content-center card-active">
-                                    <div className="col-6">
-                                        <h6 className="text-center m-b-10"><span className="text-muted m-r-5">Target:</span>25,998</h6>
-                                        <div className="progress">
-                                            <div className="progress-bar progress-c-theme" role="progressbar" style={{width: '80%', height: '6px'}} aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"/>
-                                        </div>
-                                    </div>
-                                    <div className="col-6">
-                                        <h6 className="text-center  m-b-10"><span className="text-muted m-r-5">Duration:</span>900</h6>
-                                        <div className="progress">
-                                            <div className="progress-bar progress-c-theme2" role="progressbar" style={{width: '50%', height: '6px'}} aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"/>
-                                        </div>
-                                    </div>
-                                </div>
+                                <NVD3Chart id="chart" height={300} type="pieChart" datum={datum1} x="key" y="y"  />
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col md={6} xl={6} className='m-b-30'>
+                        <Card>
+                            <Card.Header>
+                                <Card.Title as='h5'>Students Attendance Yearly Report</Card.Title>
+                            </Card.Header>
+                            <Card.Body>
+                                <NVD3Chart id="chart" height={300} type="pieChart" datum={datum2} x="key" y="y"  />
                             </Card.Body>
                         </Card>
                     </Col>
