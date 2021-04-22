072fc4c4468b12aa6c8971b79202aefefc239754
abduljeleelng <abduljeleelng@gmail.com>
2021-04-22 12:22:32 +0100

    File structuring for Admin with fake Text

--- src/route.js
+++ src/route.js
@@ -1,17 +1,29 @@
 import React from 'react';
 
-const SignUp1 = React.lazy(() => import('./Demo/Authentication/SignUp/SignUp1'));
-const Signin1 = React.lazy(() => import('./Demo/Authentication/SignIn/SignIn1'));
-
+const Home = React.lazy(()=>import('./screen/Auth/welcome/Home'))
 const AdminSignIn = React.lazy(() => import('./screen/Auth/admin/SignIn1'))
+const AdminForget = React.lazy(() => import('./screen/Auth/admin/SignIn1'))
+const AdminReset = React.lazy(() => import('./screen/Auth/admin/SignIn1'))
+
+const DSignIn = React.lazy(() => import('./screen/Auth/admin/SignIn1'))
+const DForget = React.lazy(() => import('./screen/Auth/admin/SignIn1'))
+const DReset = React.lazy(() => import('./screen/Auth/admin/SignIn1'))
+
+const SchoolSignIn = React.lazy(() => import('./screen/Auth/admin/SignIn1'))
+const SchoolForget = React.lazy(() => import('./screen/Auth/admin/SignIn1'))
+const SchoolReset = React.lazy(() => import('./screen/Auth/admin/SignIn1'))
 
 const route = [
-    { path: '/auth/', exact: true, name: 'Signup 1', component: SignUp1 },
-    { path: '/auth/admin/signup', exact: true, name: 'Signup 1', component: SignUp1 },
-    { path: '/auth/admin/signin', exact: true, name: 'Signup 1', component: AdminSignIn },
-    { path: '/auth/admin/forget', exact: true, name: 'Signup 1', component: SignUp1 },
-    { path: '/auth/admin/reset', exact: true, name: 'Signup 1', component: SignUp1 },
-    { path: '/auth/signin', exact: true, name: 'Signin 1', component: Signin1 }
+    { path: '/', exact: true, name: 'Home', component: Home },
+    { path: '/auth/admin/signin', exact: true, name: 'admin-signin', component: AdminSignIn },
+    { path: '/auth/admin/forget', exact: true, name: 'admin-forget', component: AdminForget},
+    { path: '/auth/admin/reset', exact: true, name: 'admin-reset', component: AdminReset },
+    { path: '/auth/district/signin', exact: true, name: 'd-signin', component: DSignIn },
+    { path: '/auth/district/forget', exact: true, name: 'd-forget', component: DForget },
+    { path: '/auth/district/reset', exact: true, name: 'd-reset', component: DReset},
+    { path: '/auth/school/signin', exact: true, name: 'school-signin', component: SchoolSignIn },
+    { path: '/auth/school/forget', exact: true, name: 'school-forget', component: SchoolForget },
+    { path: '/auth/school/reset', exact: true, name: 'school-reset', component: SchoolReset}
 ];
 
 export default route;
