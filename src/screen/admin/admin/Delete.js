b68c858078c8314f92c73383d9edced29ceef24c
abduljeleelng <abduljeleelng@gmail.com>
2021-05-23 17:56:56 +0100

    enhance the delete functionality

--- src/screen/admin/admin/Delete.js
+++ src/screen/admin/admin/Delete.js
@@ -3,7 +3,6 @@ import {Row, Col, Card, Button,} from 'react-bootstrap';
 import Aux from "../../../hoc/_Aux";
 import Swal from 'sweetalert2'
 import {  remove} from './api';
-//import { Redirect,  useParams} from 'react-router-dom';
 import { useParams, Redirect } from "react-router-dom";
 
 export default function Delete(props) {
@@ -11,24 +10,40 @@ export default function Delete(props) {
     let { id } = useParams();
     const [reload, setreload] = React.useState(false)
     const [error, seterror] = React.useState(false)
+    const [loading, setloading] = React.useState(true)
     const [redirectToPage, setRedirectToPage] = React.useState(false)
-
-    /*
-    const [values, setValues] = React.useState({
-        redirectToPage:false,
-        reload:false,
-        error:false
-    })
-    */
-    //const {redirectToPage, reload, error} = values;
-
- 
 
     const redirectUser = () => {
         if (redirectToPage){
             return <Redirect to="/admin/users/read" />
         }
     };
+
+    const isLoading= () => {
+        if(loading){
+            return (
+                <Aux>
+                    <Row>
+                        <Col>
+                        <Card>
+                            <Card.Header>
+                                <Card.Title as="h5">Wait !!!.</Card.Title>
+                            </Card.Header>
+                            <Card.Body>
+                                <Row>
+                                    <Col>
+                                        <h1>Requesting to Delete Admin </h1>
+                                    </Col>
+                                </Row>
+                            </Card.Body>
+                        </Card>
+                        </Col>
+                    </Row>
+                </Aux>
+            )
+        }
+    };
+
     const isError = () => {
         if(error){
             return (
@@ -57,20 +72,23 @@ export default function Delete(props) {
     const handleReload = event =>{
         event.preventDefault();
         seterror(false)
+        setloading(true)
         setreload(!reload)
     }
 
     const bootstrap = async ()=>{
+        setloading(true)
         const data = await remove(id);
         if(!data){
             Swal.fire('Oops...', 'internet server error, Please, check your network connection', 'error')
-            seterror(true)  
+            setloading(false)
+            return seterror(true)  
         }
 
         if(data.error){
             Swal.fire('Oops...', data.error, 'error')
-            seterror(true)
-            //return
+            setloading(false)
+            return seterror(true)
         }
 
         if(data.message){
@@ -86,6 +104,7 @@ export default function Delete(props) {
                 type: 'success',
                 title: data.message
             })
+            setloading(false)
             setRedirectToPage(true)
             return Swal.fire('Great', data.message, 'success');
         }  
@@ -120,6 +139,7 @@ export default function Delete(props) {
         <Aux>
             {redirectUser()}
             {isError()}
+            {isLoading()}
         </Aux>
     )
 }
