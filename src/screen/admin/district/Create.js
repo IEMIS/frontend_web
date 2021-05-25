8c045716dada0aed52941102aebda432c7f84b1f
abduljeleelng <abduljeleelng@gmail.com>
2021-05-25 13:34:42 +0100

    fixing bugs both back and front

--- src/screen/admin/district/Create.js
+++ src/screen/admin/district/Create.js
@@ -40,7 +40,7 @@ export default function Create() {
         }
         if(phone===""){ 
             setValues({...values, loading:false})
-            return Swal.fire('Oops...', 'Phone number is required', 'error');
+            return Swal.fire('Oops...', 'District Phone number is required', 'error');
         }
         if(address===""){ 
             setValues({...values, loading:false})
@@ -49,6 +49,10 @@ export default function Create() {
         if(email===""){ 
             setValues({...values, loading:false})
             return Swal.fire('Oops...', 'District email is required', 'error');
+        }
+        if(password===""){ 
+            setValues({...values, loading:false})
+            return Swal.fire('Oops...', 'Password can not be an empty value', 'error');
         }
 
         if(password !== password2) {
@@ -61,37 +65,33 @@ export default function Create() {
 
     const handleCreate =async ()=>{
         const user = {code, names, phone, email, password, password2, address, status}
-        console.log({user})
         const data = await create(user);
-        
+        console.log(data);
         if(!data){
             Swal.fire('Oops...', 'internet server error, Please, check your network connection', 'error')
             return setValues({...values, loading:false})
         }
-
         if(data.error){
             Swal.fire('Oops...', data.error, 'error')
             return setValues({...values, loading:false})
         }
-
         if(data.message){
             Swal.fire('Saved...', data.message, 'success')
-           return setValues({...values, loading:false, redirectToPage:true})
-        }
+            setValues({...values, loading:false, redirectToPage:true});
 
-        let Toast = Swal.mixin({
+            let Toast = Swal.mixin({
             toast: true,
             timerProgressBar: true,
             position: 'top-end',
             showConfirmButton: false,
             timer: 3000
-        });
-
-        return Toast.fire({
-            animation: true,
-            type: 'success',
-            title: 'Request is successful'
-        })
+           });
+            return Toast.fire({
+                animation: true,
+                type: 'success',
+                title: 'Request is successful'
+            })
+        }
     }
 
     const redirectUser = () => {
@@ -99,6 +99,20 @@ export default function Create() {
             return <Redirect to="/admin/districts/create" />
         }
     };
+
+    React.useEffect(() => {
+        const getDistrictCode = async () =>{
+            //call the endpoint for code 
+            let dcode = 'DS001'
+            setValues(v=>({...v, code:dcode}))
+        }
+        getDistrictCode();
+        /*
+        return () => {
+            //cleanup
+        }
+        */
+    }, [])
 
     return (
         <Aux>
