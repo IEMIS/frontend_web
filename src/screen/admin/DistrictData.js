ba21ee7dae6ef9bb88ea0e20219ab11c2e48c66c
abduljeleelng <abduljeleelng@gmail.com>
2021-07-06 21:48:52 +0100

    update .............

--- src/screen/admin/DistrictData.js
+++ src/screen/admin/DistrictData.js
@@ -151,6 +151,68 @@ class DistrictData extends React.Component {
                         </Col>
                     </Row>
                     <Row>
+                        <Col md={4} xl={4}>
+                            <Card>
+                                <Card.Header>
+                                    <Card.Title>
+                                        <h5>All Student Data</h5>
+                                    </Card.Title>
+                                    <Card.Title>
+                                        Total students :  {
+                                       true ? countbygender.map((data, i)=>{
+                                        let total = 0.0;
+                                        let r = data.count * 100/100
+                                        total += r
+                                        console.log(r, total)
+                                        return null
+                                    }):"data not available" }
+                                    </Card.Title>
+                                </Card.Header>
+                                <Card.Body>
+                                    <Link to="/admin/students">
+                                      <NVD3Chart id="chart" height={200} type="pieChart" datum={countbygender} x="_id" y="count"  />
+                                    </Link>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col md={4} xl={4}>
+                            <Card>
+                                <Card.Header>
+                                    <Card.Title>
+                                        <h5>School by Ownership</h5>
+                                    </Card.Title>
+                                    <Card.Title>
+                                        Total Schools :  {
+                                     true ? countbyownership.map((data, i)=>{
+                                        let total = 0;
+                                        total += data.count
+                                        return total
+                                    }):"data not available" }
+                                    </Card.Title>
+                                </Card.Header>
+                                <Card.Body>
+                                    <Link to="/admin/schools">
+                                        <NVD3Chart id="chart" height={200} type="pieChart" datum={countbyownership} x="_id" y="count"  />
+                                    </Link>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col md={4} xl={4}>
+                            <Card>
+                                <Card.Header>
+                                    <Card.Title>
+                                        <h5>Student by Religion</h5>
+                                    </Card.Title>
+                                </Card.Header>
+                                <Card.Body>
+                                  <Link to="/admin/schools">
+                                    <NVD3Chart id="chart" height={200} type="pieChart" datum={countbyownership} x="_id" y="count"  />
+                                  </Link>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                    <Row>
                         <Col md={12} xl={12}>
                             <Card className='Recent-Users'>
                                 <Card.Header>
