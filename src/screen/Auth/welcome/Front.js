6ce4a01dc606a962e7eca13c0f46d69f9be2c195
Olarotimi84 <hamasoasis84@gmail.com>
2021-09-27 21:51:05 +1200

    welcome page|footer|Features|About

--- src/screen/Auth/welcome/Front.js
+++ src/screen/Auth/welcome/Front.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import {Row, Container, Col, Card,} from 'react-bootstrap';
+import {Row, Container, Col, Card,Tabs, Tab} from 'react-bootstrap';
 //import './styles/app.scss'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 import Aux from '../../../hoc/_Aux';
 import Navigation from './components/Navigation';
+import Footer from './components/Footer';
 import Slide from './components/Slide'
 
 export default function Front() {
+
     return (
         <Aux>
             <Navigation />
@@ -22,30 +24,46 @@ export default function Front() {
                             </Card.Header>
                             <Card.Body>
                                 <p>
-                                Strengthening of Education Management Information System (EMIS) has been the focus of International Development Partners (IDPs) over the years, while developed countries leverage on proprietary School Management Software (SMS) solutions such as Alma, Class365, Edsby, Eduflex, Edvance etc. in managing their education data, these solutions are expensive and unsuitable for developing countries whose annual education budget still fall below international benchmark. Today, countries are without EMIS and those with EMIS are yet to advance from desktop solutions to a web platform for efficient coverage and handling of education data.
+                                The growing demand for valid and reliable data for planning, management of educational services and providing comparative information on performance globally led to Education Management Information System (EMIS). The outcomes are still largely poor with 75% in pacific and 81% Sub-Saharan Africa countries are using paper based data collection, despite millions of dollars spent to strengthen EMIS by donors.
+                                </p>
+                                <p>
+                                Developed countries leverage on proprietary School Management Software (SMS) solutions such as Alma, Class365, Edsby, Eduflex, Edvance etc. in managing their education data, these solutions are expensive and unsuitable for developing countries with annual education budget below recommended international benchmark.
+                                </p>
+                                <p> 
+                                Today, countries are without EMIS and those with EMIS are yet to advance from desktop solutions to a web platform for wider accesibility and efficient data management.  
+                                </p>
+                                <p>
+                                An affordable school centered integrated web solution developed by Fiji Ministry of Education, known as FEMIS provides alternatives for developing countries. However, it has core architecture design deficiencies and focuses on business model only (for managing allocation of grants to schools and provision of vouchers to students) rather than generating education statistic and indicators data that can give overall education performances in the country. 
+                                </p>
+                                <p>
+                                A modern web application Intelligent EMIS (IEMIS) built on microservices architecture using MERN (MongoDB, ExpressJS, ReactJS, and NodeJS) stack to address EMIS challenges becomes necessary. It is user-friendly, offers enhanced data management, efficient automation and visualization of education data / indicators required for educational planning and policy-making. 
                                 </p>
                             </Card.Body>
                         </Card>
                     </Col>
                     <Col xl={6} md={6} lg={6}>
-                        <Card>
+                    <Card>
                             <Card.Header>
-                                <Card.Title> Why IEMIS</Card.Title>
+                                <Card.Title> EDUCATION DATA SYNOPSIS</Card.Title>
                             </Card.Header>
                             <Card.Body>
-                                <p>
-                                Strengthening of Education Management Information System (EMIS) has been the focus of International Development Partners (IDPs) over the years, while developed countries leverage on proprietary School Management Software (SMS) solutions such as Alma, Class365, Edsby, Eduflex, Edvance etc. in managing their education data, these solutions are expensive and unsuitable for developing countries whose annual education budget still fall below international benchmark. Today, countries are without EMIS and those with EMIS are yet to advance from desktop solutions to a web platform for efficient coverage and handling of education data.
-                                </p>
-                            </Card.Body>
-                        </Card>
+                            <Tabs defaultActiveKey="current" id="uncontrolled-tab-example">
+                            <Tab eventKey="last" title="2018 Insights">
+                                
+                            </Tab>
+                            <Tab eventKey="previous" title="2019 Insights">
+                            </Tab>
+                            <Tab eventKey="current" title="2020 Insights">
+                            </Tab>
+                            <Tab eventKey="new" title="2021 Insights">
+                            </Tab>
+                        </Tabs>
+                        </Card.Body>
+                    </Card>
                     </Col>
                 </Row>
-                <Row>
-                    <Col></Col>
-                    <Col>All rights reserved @IEMIS 2021</Col>
-                    <Col></Col>
-                </Row>
             </Container>
+            <Footer />
         </Aux>
-    )
+    );
 }
---
+++ src/screen/Auth/welcome/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Container } from 'react-bootstrap';
+
+
+
+export default function Footer() {
+    return (
+            <footer collapseOnSelect sticky="top" expand="lg" bg="gray" >
+            <Container>
+            
+                <p>Copyright 2021 © <span>IEMISv1.0 </span> | All Rights Reserved</p>
+
+            </Container>
+            </footer>
+            )
+        }
+        
