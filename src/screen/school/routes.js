31efcbc2c640e99ca7e7ac1c47fbf5cabf5c7e5f
abduljeleelng <abduljeleelng@gmail.com>
2021-10-05 09:01:51 +0100

    school services

--- src/screen/school/routes.js
+++ src/screen/school/routes.js
@@ -30,26 +30,26 @@ const ReadOneStaffs = React.lazy(()=>import('./staffs/One'))
 const DeleteStaffs = React.lazy(()=>import('./staffs/Delete'))
 
 const routes = [
-    { path: '/school', exact: true, name: 'admin-district', component: SchoolData },
-    { path: '/school/indicator', exact: true, name: 'admin-indicator', component: IndicatorData },
+    { path: '/school', exact: true, name: 'school-district', component: SchoolData },
+    { path: '/school/indicator', exact: true, name: 'school-indicator', component: IndicatorData },
   
-    { path: '/school/students/create', exact: true, name: 'admin-sch-create', component: CreateStudent},
-    { path: '/school/students/read', exact: true, name: 'admin-sch-read', component: ReadStudent},
-    { path: '/school/students/read/:id', exact: true, name: 'admin-sch-read-one', component:ReadOneStudent},
-    { path: '/school/students/edit/:id', exact: true, name: 'admin-sch-edit', component: EditStudent},
-    { path: '/school/students/delete/:id', exact: true, name: 'admin-sch-delete', component: DeleteStudent},
+    { path: '/school/students/create', exact: true, name: 'school-sch-create', component: CreateStudent},
+    { path: '/school/students/read', exact: true, name: 'school-sch-read', component: ReadStudent},
+    { path: '/school/students/read/:id', exact: true, name: 'school-sch-read-one', component:ReadOneStudent},
+    { path: '/school/students/edit/:id', exact: true, name: 'school-sch-edit', component: EditStudent},
+    { path: '/school/students/delete/:id', exact: true, name: 'school-sch-delete', component: DeleteStudent},
 
-    { path: '/school/teachers/create', exact: true, name: 'admin-sch-create', component: CreateTeacher},
-    { path: '/school/teachers/read', exact: true, name: 'admin-sch-read', component: ReadTeacher},
-    { path: '/school/teachers/read/:id', exact: true, name: 'admin-sch-read-one', component:ReadOneTeacher},
-    { path: '/school/teachers/edit/:id', exact: true, name: 'admin-sch-edit', component: EditTeacher},
-    { path: '/school/teachers/delete/:id', exact: true, name: 'admin-sch-delete', component: DeleteTeacher},
+    { path: '/school/teachers/create', exact: true, name: 'school-sch-create', component: CreateTeacher},
+    { path: '/school/teachers/read', exact: true, name: 'school-sch-read', component: ReadTeacher},
+    { path: '/school/teachers/read/:id', exact: true, name: 'school-sch-read-one', component:ReadOneTeacher},
+    { path: '/school/teachers/edit/:id', exact: true, name: 'school-sch-edit', component: EditTeacher},
+    { path: '/school/teachers/delete/:id', exact: true, name: 'school-sch-delete', component: DeleteTeacher},
 
-    { path: '/school/staffs/create', exact: true, name: 'admin-sch-create', component: CreateStaffs},
-    { path: '/school/staffs/read', exact: true, name: 'admin-sch-read', component: ReadStaffs},
-    { path: '/school/staffs/read/:id', exact: true, name: 'admin-sch-read-one', component:ReadOneStaffs},
-    { path: '/school/staffs/edit/:id', exact: true, name: 'admin-sch-edit', component: EditStaffs},
-    { path: '/school/staffs/delete/:id', exact: true, name: 'admin-sch-delete', component: DeleteStaffs},
+    { path: '/school/staffs/create', exact: true, name: 'school-sch-create', component: CreateStaffs},
+    { path: '/school/staffs/read', exact: true, name: 'school-sch-read', component: ReadStaffs},
+    { path: '/school/staffs/read/:id', exact: true, name: 'school-sch-read-one', component:ReadOneStaffs},
+    { path: '/school/staffs/edit/:id', exact: true, name: 'school-sch-edit', component: EditStaffs},
+    { path: '/school/staffs/delete/:id', exact: true, name: 'school-sch-delete', component: DeleteStaffs},
 ];
 
 export default routes;
