eda0c415cea52f9683af3f1ed184b93256f42ee2
abduljeleelng <abduljeleelng@gmail.com>
2021-10-05 13:51:20 +0100

    teacher services

--- src/screen/school/teachers/api/index.js
+++ src/screen/school/teachers/api/index.js
@@ -1,8 +1,8 @@
 //let  BASE_URL = "",
 
-const BASE_URL = "https://iemis.herokuapp.com/api/v1/district"
+//const BASE_URL = "https://iemis.herokuapp.com/api/v1/schools"
 
-// const BASE_URL = "http://localhost:9000/api/v1/district" 
+const BASE_URL = "http://localhost:9000/api/v1/schools" 
 
 export const create = async (user, token)=>{
     try{
